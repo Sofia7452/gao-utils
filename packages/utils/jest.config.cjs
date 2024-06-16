385f213b58200398d7c45f0614edf4154c3c0db7
gaojia <sofia7452@163.com>
2024-06-16 15:18:58 +0800

    feat: 使用ts(有问题)

--- packages/utils/jest.config.cjs
+++ packages/utils/jest.config.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   testMatch: ["**/__tests__/**/*.js", "**/?(*.)+(spec|test).js"],
   transform: { // 类似 webpack.module.rules, 转换规则
-    '^.+\\.js': 'babel-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest',
   },
 };
